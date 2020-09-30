3f32229599dab966e726af5d11f97875d39c47e3
heysivani <heysivani@gmail.com>
2020-09-30 12:21:20 +0900

    getElementsByClassName basic structure completed
    Co-authored-by: ninamatti <ninamatti@users.noreply.github.com>
    Co-authored-by: heysivani <heysivani@users.noreply.github.com>

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,21 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (target) => {
+  let output = [];
+  let topNodes = document.children;
+  function traverse(nodes) {
+    for (const node of nodes) {
+      if (node.classList === target) {
+        output.push(node);
+      }
+      if (node.children === undefined) {
+        continue;
+      }
+      let kids = node.children;
+      traverse(kids);
+    }
+    return;
+  }
+  output = traverse(topNodes);
+  return output;
 };
 
 module.exports = { getElementsByClassName };
